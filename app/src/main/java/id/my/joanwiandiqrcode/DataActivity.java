6550889ea90787094e40eb4669adb5fa7577d310
Jw Techno <joanwiandi@gmail.com>
2021-07-27 21:55:22 +0700

    uas

---
+++ app/src/main/java/id/my/joanwiandiqrcode/DataActivity.java
@@ -0,0 +1,1 @@
+package id.my.joanwiandiqrcode;import android.os.Bundle;import androidx.appcompat.app.AppCompatActivity;import id.my.joanwiandiqrcode.R;public class DataActivity extends AppCompatActivity {    @Override    protected void onCreate(Bundle savedInstanceState) {        super.onCreate(savedInstanceState);        setContentView(R.layout.activity_data);    }}
---
+++ app/src/main/java/id/my/joanwiandiqrcode/ScanActivity.java
@@ -0,0 +1,1 @@
+package id.my.joanwiandiqrcode;import android.os.Bundle;import androidx.appcompat.app.AppCompatActivity;import id.my.joanwiandiqrcode.R;public class ScanActivity extends AppCompatActivity {    @Override    protected void onCreate(Bundle savedInstanceState) {        super.onCreate(savedInstanceState);        setContentView(R.layout.activity_scan);    }}
